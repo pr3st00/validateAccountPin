52133ce532beb51b062817d2ad901a27b89a7fe5
Fernando Almeida <fernando.x.almeida@t-mobile.com>
2023-08-10 12:52:11 -0300

    Return 404 for an invalid pin

--- src/main/java/com/ibm/watsonx/ivr/validateAccountPin/user/UserResource.java
+++ src/main/java/com/ibm/watsonx/ivr/validateAccountPin/user/UserResource.java
@@ -1,23 +1,29 @@
 package com.ibm.watsonx.ivr.validateAccountPin.user;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class UserResource {
-	
+
 	private UserDaoService service;
 
 	public UserResource(UserDaoService service) {
 		super();
 		this.service = service;
 	}
-	
-	@GetMapping(path="/validate/{pin}") //validate pin
+
+	@GetMapping(path = "/validate/{pin}")
 	public boolean validatePin(@PathVariable String pin) {
 		boolean isValid = service.validatePin(pin);
-		
+
+		if (!isValid) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "pin is invalid");
+		}
+
 		return isValid;
 	}
 
